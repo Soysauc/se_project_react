892a22e94da29ba270853078edc9aa77a0d3eba6
Jeremy M Rivera <fangman09@gmail.com>
2022-11-26 19:38:08 -0500

    started header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,27 @@
-// A child component of App.js. The component includes:
+// A child component of Header.js. The component includes:
 // A logo
-// The current date that can be generated using the Date() object:
+
 const currentDate = new Date().toLocaleString("default", {
   month: "long",
   day: "numeric",
 });
+
+//florida is a placeholder
+function Header() {
+  return (
+    <ul className="header">
+      <li className="header__logo">
+        <img src="../Images/wtwr-logo.svg" alt="What-to-wear-Logo"></img>
+      </li>
+      <li className="header__text"> {currentDate}, Florida </li>
+      <li className="header__text"> + Add clothes</li>
+      <li className="header__text"> Terrence Tegegne</li>
+      <li className="header__avatar"></li>
+    </ul>
+  );
+}
+
+export default Header;
 
 // The current location (see Section 4 for details)
 // An “Add Clothes” button that opens ModalWithForm
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "../src/components/App";
+import App from "./components/App/App.js";
+import Header from "./components/Header/Header.js";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <Header />
   </React.StrictMode>
 );
 
