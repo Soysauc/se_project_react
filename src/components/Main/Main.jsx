6e2283756a903214013f7610bf224e8cf15a092e
Jeremy M Rivera <fangman09@gmail.com>
2023-01-28 05:41:13 -0500

    rendered app / errors

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,22 +1,31 @@
 import "./Main.css";
+import React, { useContext } from "react";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
+
 import ItemCard from "../ItemCard/ItemCard";
 import WeatherCard from "../WeatherCard/WeatherCard";
 
-function Main({ weatherData, defaultClothing, handleCardClick }) {
-  const currentWeather = weatherData.temperature;
+function Main({ weatherData, clothingItems, handleCardClick }) {
+  // const currentWeather = weatherData.temperature;
+  const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
+
+  const currentTemp =
+    currentTemperatureUnit === "F"
+      ? weatherData?.temperature?.F
+      : weatherData?.temperature?.C;
 
   const HOT_WEATHER = 86;
   const COLD_WEATHER = 64;
 
   const getWeatherType = () => {
-    if (currentWeather >= HOT_WEATHER) {
+    if (currentTemp >= HOT_WEATHER) {
       return "hot";
     } else if (
-      currentWeather >= COLD_WEATHER - 1 &&
-      currentWeather <= HOT_WEATHER - 1
+      currentTemp >= COLD_WEATHER - 1 &&
+      currentTemp <= HOT_WEATHER - 1
     ) {
       return "warm";
-    } else if (currentWeather <= COLD_WEATHER) {
+    } else if (currentTemp <= COLD_WEATHER) {
       return "cold";
     }
   };
@@ -25,13 +34,13 @@ function Main({ weatherData, defaultClothing, handleCardClick }) {
     return card.weather === getWeatherType();
   }
 
-  const clothingChoices = defaultClothing.filter(filterClothing);
+  const clothingChoices = clothingItems.filter(filterClothing);
 
   return (
     <main className="main">
       <WeatherCard weatherData={weatherData} />
       <h3 className="main__header">
-        Today is {Math.round(currentWeather)}&deg;F / You may want to wear:
+        Today is {Math.round(currentTemp)}&deg;F / You may want to wear:
       </h3>
       <ul className="main__gallery">
         {clothingChoices.map((item) => (
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -4,7 +4,7 @@ import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnit
 const DAY_HOUR = 6;
 const NIGHT_HOUR = 17;
 
-function WeatherCard({ weatherData }) {
+function WeatherCard({ weatherData, currentTemp }) {
   const { currentTemperatureUnit } = useContext(CurrentTemperatureUnitContext);
 
   if (!weatherData) return null;
@@ -60,7 +60,7 @@ function WeatherCard({ weatherData }) {
       )}_${checkForRain()}`}
     >
       <h2 className="weathercard__temp">
-        {weatherData.temperature[currentTemperatureUnit]}
+        {` ${currentTemp}°${currentTemperatureUnit} `}
       </h2>
       <div className="weathercard__image-wrWeatherCarder">
         <img
